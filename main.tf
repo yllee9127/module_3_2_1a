71471e2d17541f2a0e0a13697dba868b6e0c2cf8
yllee9127 <yllee9127@gmail.com>
2025-02-06 21:53:45 +0800

    Run fmt again

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ terraform {
   required_version = ">= 1.9"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
